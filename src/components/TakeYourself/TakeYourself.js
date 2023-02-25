96b1e725b1244b55ed6e91b4a325fab6d4ba1b60
Good_Cthulhu <hayyabusa_01@mail.ru>
2023-02-25 21:40:29 +0300

    fix

--- src/components/TakeYourself/TakeYourself.js
+++ src/components/TakeYourself/TakeYourself.js
@@ -1,47 +1,40 @@
-import React, { useRef } from 'react';
+import React from 'react';
 
 import { Container, Title } from '../../globalStyle';
+
 import {
-  YourselfWrapper,
-  YourselfImage,
   YourselfItem,
-  YourselfTitle,
-  YourselfText,
+  YourselfWrapper,
+  YourselfImageWrapper,
+  YourselfImage,
   YourselfContent,
+  PetName,
+  PetEyesColor,
+  PetWoolType,
 } from './TakeYourselfStyle';
+
 
 import { data } from '../../data/YourselfData';
 
-import { useScroll, useTransform } from 'framer-motion';
 
 const TakeYourself = () => {
-  function Parallax({ elem }) {
-    const ref = useRef(null);
-    const { scrollYProgress } = useScroll({ target: ref });
-    const y = useTransform(scrollYProgress, [0, 1], [-300, 300]);
-    console.log(scrollYProgress);
-    return (
-      <YourselfItem>
-        <YourselfImage bgimage={elem.imageUrl} ref={ref} />
-        <YourselfContent style={{ y }}>
-          <YourselfTitle>{elem.title}</YourselfTitle>
-          <YourselfText>{elem.text}</YourselfText>
-        </YourselfContent>
-      </YourselfItem>
-    );
-  }
   return (
-    <Container maxWidth="100%" bgcolor={'rgba(0,0,0,0.2);'} p="40px">
-      <Title mb="20px">Забери меня</Title>
-      <Container>
-        <YourselfWrapper>
-          {data
-            .sort(() => Math.random() - 0.5)
-            .map((elem, index) => {
-              return <Parallax elem={elem} key={index} />;
-            })}
-        </YourselfWrapper>
-      </Container>
+    <Container>
+      <Title>Я ищу дом</Title>
+      <YourselfWrapper>
+        {data.map((elem, index) => {
+        return  <YourselfItem>
+            <YourselfImageWrapper>
+              <YourselfImage bgimage={elem.imageUrl}/>
+            </YourselfImageWrapper>
+            <YourselfContent>
+              <PetName></PetName>
+              <PetEyesColor></PetEyesColor>
+              <PetWoolType></PetWoolType>
+            </YourselfContent>
+          </YourselfItem>;
+        })}
+      </YourselfWrapper>
     </Container>
   );
 };
--- src/components/TakeYourself/TakeYourselfStyle.js
+++ src/components/TakeYourself/TakeYourselfStyle.js
@@ -1,47 +1,25 @@
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
 
-export const YourselfWrapper = styled(motion.div)`
-  width: 100%;
-  height: 900px;
-  overflow: scroll;
-  scroll-snap-type: y mandatory;
+export const YourselfWrapper = styled.div`
+  margin-top: 50px;
 `;
 
-export const YourselfItem = styled(motion.div)`
-  position: relative;
-  margin: 50px 0;
-  scroll-snap-align: start;
-`;
+export const YourselfItem = styled.div``;
 
-export const YourselfImage = styled(motion.div)`
-  background-image: url(${({ bgimage }) => bgimage || ''});
-  background-position: top center;
-  object-fit: cover;
+export const YourselfImageWrapper = styled.div``;
+export const YourselfImage = styled.img`
+  background-image: url(${({bgimage}) => bgimage || ''});
   background-repeat: no-repeat;
   background-size: cover;
-  height: 900px;
-`;
-
-export const YourselfTitle = styled.h3`
-  margin-bottom: 7px;
-`;
-
-export const YourselfText = styled.p`
-  font-size: 16px;
-  border: 1px solid #fff;
-  padding: 5px;
-  border-radius: 4px;
-`;
-
-export const YourselfContent = styled(motion.div)`
   width: 300px;
-  padding: 30px;
-  border-radius: 5px;
-  background-color: rgba(0, 0, 0, 0.5);
-  text-align: left;
-
-  position: absolute;
-  top: 50%;
-  right: 20px;
+  height: 300px;
+  background-position: top center;
 `;
+
+export const YourselfContent = styled.div``;
+
+export const PetName = styled.p``;
+
+export const PetEyesColor = styled.p``;
+export const PetWoolType = styled.p``;
